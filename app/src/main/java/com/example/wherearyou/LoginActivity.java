e1d80743246e5c9017b3d6d008fcdaeb543b4727
donguk <ldu2175@gmail.com>
2019-07-18 01:41:05 +0900

    Add UI MainActivity
    Add UI MainActivity

--- app/src/main/java/com/example/wherearyou/LoginActivity.java
+++ app/src/main/java/com/example/wherearyou/LoginActivity.java
@@ -1,8 +1,12 @@
 package com.example.wherearyou;
 
+import android.content.Intent;
 import android.media.MediaPlayer;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.VideoView;
 import android.net.Uri;
 
@@ -12,14 +16,28 @@ import android.net.Uri;
 
 public class LoginActivity extends AppCompatActivity {
     private VideoView videoBG;
+    private Button Sign_in_btn;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        // 백그라운드 영상 재생 함수
+        // 백그라운드 영상 재생
         playBackgroundVideo();
+
+        // SIGN IN 버튼을 누를 시 메인 화면으로 전환
+        Sign_in_btn = (Button)findViewById(R.id.sign_in_btn);
+        Sign_in_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick (View v) {
+                // TODO: 로그인 연동 (김청은)
+
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
     // 백그라운드 영상 재생 함수
--- app/src/main/java/com/example/wherearyou/MainActivity.java
+++ app/src/main/java/com/example/wherearyou/MainActivity.java
@@ -1,19 +1,48 @@
 package com.example.wherearyou;
 
-import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
-/*
-    메인화면 기능을 관리하는 클래스
-*/
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.annotation.NonNull;
+
+import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    // 하단 네비게이션 바
+    private TextView mTextMessage;
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:       // 홈 화면
+                    mTextMessage.setText(R.string.title_home);
+                    return true;
+                case R.id.navigation_friends:    // 친구 화면
+                    mTextMessage.setText(R.string.title_friends);
+                    return true;
+                case R.id.navigation_setting:    // 설정 화면
+                    mTextMessage.setText(R.string.title_setting);
+                    return true;
+            }
+            return false;
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // 하단 내비게이션 바 표시
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        mTextMessage = findViewById(R.id.message);
+        navView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
 
+        // TODO: 지도 연동 (김학률)
     }
 }
