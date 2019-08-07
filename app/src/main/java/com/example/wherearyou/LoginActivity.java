c26ba74056f272c6f0317f1c41e7ef88015f8d36
donguk <ldu2175@gmail.com>
2019-08-07 23:09:32 +0900

    Firebase 연동, Google Login 연동
    Firebase 연동, Google Login 연동

--- app/src/main/java/com/example/wherearyou/LoginActivity.java
+++ app/src/main/java/com/example/wherearyou/LoginActivity.java
@@ -2,19 +2,45 @@ package com.example.wherearyou;
 
 import android.content.Intent;
 import android.media.MediaPlayer;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 import android.widget.VideoView;
 import android.net.Uri;
+
+
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.GoogleAuthProvider;
+
 
 /*
     로그인화면 기능을 관리하는 클래스
 */
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener {
+    SignInButton Google_Login;
+    private static final int RC_SIGN_IN = 100;
+    private FirebaseAuth mAuth;
+    private GoogleApiClient mGoogleApiClient;
+
     private VideoView videoBG;
     private Button Sign_in_btn;
 
@@ -26,15 +52,26 @@ public class LoginActivity extends AppCompatActivity {
         // 백그라운드 영상 재생
         playBackgroundVideo();
 
-        // SIGN IN 버튼을 누를 시 메인 화면으로 전환
-        Sign_in_btn = (Button)findViewById(R.id.sign_in_btn);
-        Sign_in_btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick (View v) {
-                // TODO: 로그인 연동 (김청은)
+        // 구글 로그인 옵션 객체 : 옵션을 관리해주는 클래스로 API 키 값과 요청할 값이 저장되어 있음.
+        // 이를 이용해 Google API 클라이언트 생성
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this, this)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
 
-                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-                startActivity(intent);
+        mAuth = FirebaseAuth.getInstance();
+        Google_Login = findViewById(R.id.Google_Login);
+
+        Google_Login.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view){
+                // API로부터 인텐트를 불러와 액티비티를 시작
+                Toast.makeText(LoginActivity.this, "액티비티 실행", Toast.LENGTH_SHORT).show();
+                Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+                startActivityForResult(signInIntent, RC_SIGN_IN);
             }
         });
     }
@@ -52,5 +89,43 @@ public class LoginActivity extends AppCompatActivity {
                 mediaPlayer.setLooping(true);
             }
         });
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == RC_SIGN_IN){
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            Toast.makeText(LoginActivity.this, "ActivityResult Run", Toast.LENGTH_SHORT).show();
+            // 로그인 성공 시
+            if(result.isSuccess()){
+                GoogleSignInAccount account = result.getSignInAccount();
+                firebaseAuthWithGoogle(account);
+            } else {
+                Toast.makeText(LoginActivity.this, "구글 로그인 실패", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private void firebaseAuthWithGoogle(GoogleSignInAccount acct){
+        AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(),null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(!task.isSuccessful()){
+                            Toast.makeText(LoginActivity.this, "인증 실패", Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(LoginActivity.this, "구글 로그인 인증 성공", Toast.LENGTH_SHORT).show();
+                            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                            startActivity(intent);
+                        }
+                    }
+                });
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
     }
 }
