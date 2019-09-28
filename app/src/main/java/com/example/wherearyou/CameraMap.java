fad6131387bd78d065b318dbdf50bd0e7effd9dc
donguk <ldu2175@gmail.com>
2019-09-28 20:58:21 +0900

    Opengl 설치, AR 일정 거리의 허공에 물체 띄우기 구현

--- app/src/main/java/com/example/wherearyou/CameraMap.java
+++ app/src/main/java/com/example/wherearyou/CameraMap.java
@@ -1,96 +1,300 @@
 package com.example.wherearyou;
 
 import android.Manifest;
+import android.app.Activity;
 import android.content.pm.PackageManager;
-import android.hardware.Camera;
-import android.os.Build;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
-import androidx.appcompat.app.AppCompatActivity;
+import android.hardware.display.DisplayManager;
+import android.opengl.GLSurfaceView;
+import android.opengl.Matrix;
 import android.os.Bundle;
-import android.view.SurfaceHolder;
+import android.util.Log;
+import android.view.Display;
+import android.view.MotionEvent;
 import android.view.Window;
 import android.view.WindowManager;
-import android.widget.Button;
+import android.widget.CheckBox;
 
-public class CameraMap extends AppCompatActivity {
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
-    private static CameraPreview surfaceView;
-    private SurfaceHolder holder;
-    private static Button camera_preview_button;
-    private static Camera mCamera;
-    private int RESULT_PERMISSIONS = 100;
-    public static MainActivity getInstance;
+import com.google.ar.core.ArCoreApk;
+import com.google.ar.core.Camera;
+import com.google.ar.core.Config;
+import com.google.ar.core.Frame;
+import com.google.ar.core.PointCloud;
+import com.google.ar.core.Pose;
+import com.google.ar.core.Session;
+import com.google.ar.core.exceptions.CameraNotAvailableException;
+import com.google.ar.core.exceptions.UnavailableApkTooOldException;
+import com.google.ar.core.exceptions.UnavailableArcoreNotInstalledException;
+import com.google.ar.core.exceptions.UnavailableDeviceNotCompatibleException;
+import com.google.ar.core.exceptions.UnavailableSdkTooOldException;
+import com.google.ar.core.exceptions.UnavailableUserDeclinedInstallationException;
+
+public class CameraMap extends Activity {
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    private CheckBox mCheckBox;
+    private GLSurfaceView mSurfaceView;
+    private MainRenderer mRenderer;
+
+    private boolean mUserRequestedInstall = true;
+
+    private Session mSession;
+    private Config mConfig;
+
+    private float[] mProjMatrix = new float[16];
+    private float[] mViewMatrix = new float[16];
+
+    private float mLastX;
+    private float mLastY;
+    private float[] mLastPoint = new float[] { 0.0f, 0.0f, 0.0f };
+    private boolean mNewPath = false;
+    private boolean mPointAdded = false;
+
+    private static float MIN_DISTANCE = 0.000625f;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // 카메라 프리뷰를  전체화면으로 보여주기 위해 셋팅한다.
-        supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                WindowManager.LayoutParams.FLAG_FULLSCREEN);
-
-
-        // 안드로이드 6.0 이상 버전에서는 CAMERA 권한 허가를 요청한다.
-        requestPermissionCamera();
-    }
-    public static Camera getCamera(){
-        return mCamera;
-    }
-    private void setInit(){
-
-        // 카메라 객체를 R.layout.activity_main의 레이아웃에 선언한 SurfaceView에서 먼저 정의해야 함으로 setContentView 보다 먼저 정의한다.
-        mCamera = Camera.open();
-
+        hideStatusBarAndTitleBar();
         setContentView(R.layout.activity_camera_map);
 
-        // SurfaceView를 상속받은 레이아웃을 정의한다.
-        surfaceView = (CameraPreview) findViewById(R.id.preview);
+        mSurfaceView = (GLSurfaceView) findViewById(R.id.gl_surface_view);
 
+        DisplayManager displayManager = (DisplayManager) getSystemService(DISPLAY_SERVICE);
+        if (displayManager != null) {
+            displayManager.registerDisplayListener(new DisplayManager.DisplayListener() {
+                @Override
+                public void onDisplayAdded(int displayId) {
+                }
 
-        // SurfaceView 정의 - holder와 Callback을 정의한다.
-        holder = surfaceView.getHolder();
-        holder.addCallback(surfaceView);
-        holder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
-    }
+                @Override
+                public void onDisplayChanged(int displayId) {
+                    synchronized (this) {
+                        mRenderer.onDisplayChanged();
+                    }
+                }
 
-    public boolean requestPermissionCamera(){
-        int sdkVersion = Build.VERSION.SDK_INT;
-        if(sdkVersion >= Build.VERSION_CODES.M) {
-
-            if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
-
-                ActivityCompat.requestPermissions(CameraMap.this,
-                        new String[]{Manifest.permission.CAMERA},
-                        RESULT_PERMISSIONS);
-
-            }else {
-                setInit();
-            }
-        }else{  // version 6 이하일때
-            setInit();
-            return true;
+                @Override
+                public void onDisplayRemoved(int displayId) {
+                }
+            }, null);
         }
 
+        mRenderer = new MainRenderer(new MainRenderer.RenderCallback() {
+            @Override
+            // 순서 : 디스플레이 방향에 맞는 설정 ->
+            // Renderer에서 생성한 텍슻쳐를 Session 객체와 연결 ->
+            // 새로운 프레임이 그려질 때마다 Session 객체 업데이트
+            public void preRender() {
+                if (mRenderer.isViewportChanged()) {
+                    Display display = getWindowManager().getDefaultDisplay();
+                    int displayRotation = display.getRotation();
+                    // 디스플레이 방향 설정
+                    mRenderer.updateSession(mSession, displayRotation);
+                }
+
+                // Renderer에서 생성한 텍스쳐를 Session 객체와 연결
+                mSession.setCameraTextureName(mRenderer.getTextureId());
+
+                // 프레임 업데이트
+                // 카메라 영상, 포인트 클라우드 정보, 디바이스 포즈 등
+                Frame frame = null;
+                try {
+                    frame = mSession.update();
+                } catch (CameraNotAvailableException e) {
+                    e.printStackTrace();
+                }
+                if (frame.hasDisplayGeometryChanged()) {
+                    // 텍스쳐의 좌표를 디스플레이 방향에 맞게 설정해 주는 함수
+                    mRenderer.transformDisplayGeometry(frame);
+                }
+
+                // 포인트 클라우드 (현재 프레임에서 추출된 특징 점들의 3차원 좌표값)
+                PointCloud pointCloud = frame.acquirePointCloud();
+                mRenderer.updatePointCloud(pointCloud);
+                pointCloud.release();
+
+                Camera camera = frame.getCamera();
+                camera.getProjectionMatrix(mProjMatrix, 0, 0.1f, 100.0f);
+                camera.getViewMatrix(mViewMatrix, 0);
+
+                mRenderer.setProjectionMatrix(mProjMatrix);
+                mRenderer.updateViewMatrix(mViewMatrix);
+
+
+                // 스크린 상의 좌표 보다 조금 앞에 있는 터치된 3차원 좌표 값을 구하는 함수(허공)
+                //TODO : GPS 친구 좌표 -> 화면의 방향 좌표로 전환
+                //TODO : 친구좌표를 전달
+                float[] screenPoint = getScreenPoint(mLastX, 300.0f,
+                        mRenderer.getWidth(), mRenderer.getHeight(),
+                        mProjMatrix, mViewMatrix);
+
+                float[] t = screenPoint;
+                float[] r = new float[4];
+                r[0]=0.0f; r[1]=0.0f; r[2]=0.0f; r[3]=0.0f;
+
+                Pose pose = new Pose(t, r);
+                float[] modelMatrix = new float[16];
+                pose.toMatrix(modelMatrix, 0);
+                mRenderer.setCubeModelMatrix(modelMatrix);
+            }
+        });
+        mSurfaceView.setPreserveEGLContextOnPause(true);
+        mSurfaceView.setEGLContextClientVersion(2);
+        mSurfaceView.setRenderer(mRenderer);
+        mSurfaceView.setRenderMode(GLSurfaceView.RENDERMODE_CONTINUOUSLY);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        mSurfaceView.onPause();
+        mSession.pause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        requestCameraPermission();
+
+        try {
+            if (mSession == null) {
+                // ARCore SDK 설치 유무 확인
+                switch (ArCoreApk.getInstance().requestInstall(this, mUserRequestedInstall)) {
+                    case INSTALLED:
+                        mSession = new Session(this);
+                        Log.d(TAG, "ARCore 세션 연결");
+                        break;
+                    case INSTALL_REQUESTED:
+                        mUserRequestedInstall = false;
+                        Log.d(TAG, "ARCore를 설치하셔아 합니다.");
+                        break;
+                }
+            }
+        }
+        catch (UnsupportedOperationException e) {
+            Log.e(TAG, e.getMessage());
+        } catch (UnavailableApkTooOldException e) {
+            e.printStackTrace();
+        } catch (UnavailableDeviceNotCompatibleException e) {
+            e.printStackTrace();
+        } catch (UnavailableUserDeclinedInstallationException e) {
+            e.printStackTrace();
+        } catch (UnavailableArcoreNotInstalledException e) {
+            e.printStackTrace();
+        } catch (UnavailableSdkTooOldException e) {
+            e.printStackTrace();
+        }
+
+        mConfig = new Config(mSession);
+        if (!mSession.isSupported(mConfig)) {
+            Log.d(TAG, "이 기기는 ARCore를 지원하지 않습니다.");
+        }
+        mSession.configure(mConfig);
+        try {
+            mSession.resume();
+        } catch (CameraNotAvailableException e) {
+            e.printStackTrace();
+        }
+
+        mSurfaceView.onResume();
+    }
+
+    // 터치로 객체 이동하는 함수 (디버그 용)
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        mLastX = event.getX();
+        mLastY = event.getY();
+        Log.d("mLastX : " , Float.toString(mLastX));
+        Log.d("mLastY : " , Float.toString(mLastY));
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                mNewPath = true;
+                mPointAdded = true;
+                break;
+            case MotionEvent.ACTION_MOVE:
+                mPointAdded = true;
+                break;
+            case MotionEvent.ACTION_UP:
+                mPointAdded = false;
+                break;
+        }
         return true;
     }
 
+    // 스크린 상의 좌표보다 조금 앞에 있는 3차원 좌표 값을 구하는 함수
+    public float[] getScreenPoint(float x, float y, float w, float h,
+                                  float[] projMat, float[] viewMat) {
+        float[] position = new float[3];
+        float[] direction = new float[3];
 
-    @Override
-    public void onRequestPermissionsResult(int requestCode,
-                                           String permissions[], int[] grantResults) {
+        x = x * 2 / w - 1.0f;
+        y = (h - y) * 2 / h - 1.0f;
 
-        if (RESULT_PERMISSIONS == requestCode) {
+        float[] viewProjMat = new float[16];
+        Matrix.multiplyMM(viewProjMat, 0, projMat, 0, viewMat, 0);
 
-            if (grantResults.length > 0
-                    && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                // 권한 허가시
-                setInit();
-            } else {
-                // 권한 거부시
-            }
-            return;
+        float[] invertedMat = new float[16];
+        Matrix.setIdentityM(invertedMat, 0);
+        Matrix.invertM(invertedMat, 0, viewProjMat, 0);
+
+        float[] farScreenPoint = new float[]{x, y, 1.0F, 1.0F};
+        float[] nearScreenPoint = new float[]{x, y, -1.0F, 1.0F};
+        float[] nearPlanePoint = new float[4];
+        float[] farPlanePoint = new float[4];
+
+        Matrix.multiplyMV(nearPlanePoint, 0, invertedMat, 0, nearScreenPoint, 0);
+        Matrix.multiplyMV(farPlanePoint, 0, invertedMat, 0, farScreenPoint, 0);
+
+        position[0] = nearPlanePoint[0] / nearPlanePoint[3];
+        position[1] = nearPlanePoint[1] / nearPlanePoint[3];
+        position[2] = nearPlanePoint[2] / nearPlanePoint[3];
+
+        direction[0] = farPlanePoint[0] / farPlanePoint[3] - position[0];
+        direction[1] = farPlanePoint[1] / farPlanePoint[3] - position[1];
+        direction[2] = farPlanePoint[2] / farPlanePoint[3] - position[2];
+
+        normalize(direction);
+
+        position[0] += (direction[0] * 0.1f);
+        position[1] += (direction[1] * 0.1f);
+        position[2] += (direction[2] * 0.1f);
+
+        return position;
+    }
+
+    private void normalize(float[] v) {
+        double norm = Math.sqrt(v[0] * v[0] + v[1] * v[1] + v[2] * v[2]);
+        v[0] /= norm;
+        v[1] /= norm;
+        v[2] /= norm;
+    }
+
+    public boolean checkDistance(float x1, float y1, float z1, float x2, float y2, float z2) {
+        float x = x1 - x2;
+        float y = y1 - y2;
+        float z = z1 - z2;
+        return (Math.sqrt(x*x + y*y + z*z) > MIN_DISTANCE);
+    }
+
+
+    // 카메라 권한 부여 함수
+    private void requestCameraPermission() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.CAMERA}, 0);
         }
+    }
 
+    // 타이틀 바 제거
+    private void hideStatusBarAndTitleBar() {
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
     }
 }
